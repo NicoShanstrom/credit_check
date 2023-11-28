b7214d84d11cd183251b774aeda359b70a5d794d
Nico Shanstrom <nicoshanstrom@gmail.com>
2023-11-27 21:27:11 -0500

    Add an algorithm

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,7 +1,33 @@
-card_number = "5541808923795240"
+
 
 # Your Luhn Algorithm Here
+def is_luhn_valid?(card_number)
+ double_eod = 
+ double_eod > 9 add digits
+ if sum_all_digits % 10
+    puts = "The number is valid."   
+end
+end
+card_number="5541808923795240"
 
+def luhn_valid?(card_number)
+  cc_digits = card_number.to_s.reverse.chars.map(&:to_i)
+  check_sum = 0
+
+  cc_digits.each_slice(2) do |odd, even|
+    check_sum += odd
+    next unless even
+    even *= 2
+    even = even.divmod(10).inject(:+) if even > 9
+    check_sum += even
+    return check_sum.modulo(10) == 0
+    if luhn_valid? == true
+        p "The number #{card_number} is valid."
+    else 
+        p "The number #{card_number} is invalid."
+  end
+end
+luhn_valid?("5541808923795240")
 # Output
 ## If it is valid, print "The number [card number] is valid!"
 ## If it is invalid, print "The number [card number] is invalid!"
